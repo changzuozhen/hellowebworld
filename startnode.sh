50209c4c610a836295eac646a713e80a7629f1d8
changzuozhen <changzuozhen@didichuxing.com>
2019-07-03 09:09:26 +0800

    init

---
+++ startnode.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+lsof -i:80 | grep node | awk '{print $2}' | xargs kill
+#ps -e | grep node | awk '{print $1}' | xargs kill -9
+echo after kill
+ps
+# node app.js > log.txt &
+npm start
+echo after restart
+ps
+ippath=`ifconfig en0 | grep inet | grep -v inet6 | cut -d " " -f2 `;
+echo -e "visit \033[44;37;5m http://"$ippath" \033[0m\n"
